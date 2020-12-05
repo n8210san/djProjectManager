cb416ff78ba9ff12720941b0bc7b566c935a2e84
n8210san <n8210san@gmail.com>
2020-12-05 09:37:05 +0800

    commit add catalog

---
+++ catalog/tests.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+
+# Create your tests here.
+class abc():
+    title='test1'
+    gg='2'
+    def hollow(self):
+        print('hi')
+class abc():
+    title='test2'
+    gg='33333'
+    def hollow(self):
+        print('hellow kitty')
+
+a= abc()
+a.hollow()
+
+
+
+
+
+
+
---
+++ catalog/urls.py
@@ -0,0 +1,12 @@
+
+""" 2020 1128 練習
+ https://developer.mozilla.org/zh-TW/docs/Learn/Server-side/Django/skeleton_website
+ 最後一步，在catalog文件夾中，創建一個名為urls.py的文件，
+ 並添加以下文本，以定義（空）導入的urlpatterns。
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+]
